e28a653347194d7d95cc699605a624f27a560348
willis plummer <willisplummer@gmail.com>
2018-02-12 12:21:14 -0500

    send avatarurl and nickname

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import socketIOClient from "socket.io-client";
+import './App.css';
 
 const endpoint = 'http://localhost:4001'
 
@@ -22,18 +23,28 @@ class App extends React.Component {
   }
 
   onSubmit() {
-    socket.emit('data', this.state.input)
+    const body = this.state.input
+    const nickname = 'willis'
+    const avatarUrl = 'https://hwhome.com/content/images/thumbs/0005759_water-jug-man_600.jpeg'
+    socket.emit('data', { body, nickname, avatarUrl })
     this.setState({input: ''})
   }
 
   render() {
     return (
       <div style={{ textAlign: "center" }}>
-        <input value={this.state.input} onChange={this.onInputChange}/>
-        <button onClick={this.onSubmit}>test</button>
-        {this.state.chat.map(message =>
-          <div key={`chat-${message.id}`}>{message.body}</div>)
-        }
+        <div className="messages">
+          {this.state.chat.map((message, i) =>
+            <div className={i%2 === 0 ? "message white" : "message lightblue"} key={`chat-${message.id}`}>
+              <img ref={message.id} alt="avatar" className="avatar" src={message.avatar} onError={(e) => {this.refs[message.id].src = 'http://s3.amazonaws.com/37assets/svn/765-default-avatar.png'}} />
+              <span className="message-body">{message.body}</span>
+            </div>)
+          }
+        </div>
+        <div className="message-input">
+          <textarea rows="7" value={this.state.input} onChange={this.onInputChange}/>
+          <button onClick={this.onSubmit}>submit</button>
+        </div>
       </div>
     )
   }
