2db7336100d47e399c54f3c2b301b045b359d883
willis plummer <willisplummer@gmail.com>
2018-02-13 06:09:48 -0500

    implement avatar management

--- src/App.js
+++ src/App.js
@@ -2,32 +2,59 @@ import React from "react";
 import socketIOClient from "socket.io-client";
 import './App.css';
 
-const endpoint = 'http://localhost:4001'
+// const endpoint = 'http://localhost:4001'
+const endpoint = 'https://cryptic-plains-73206.herokuapp.com/'
 
-const socket = socketIOClient(endpoint)
+const room = window.location.hash ? window.location.hash.substr(1) : 'general'
+const socket = socketIOClient(endpoint, { query: `room=${room}`})
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {chat: [], input: ''}
+    this.state = {
+      avatarUrl: '',
+      avatarUrlInput: '',
+      chat: [],
+      editingAvatar: false,
+      input: ''
+    }
     socket.on('update chat', chat => {
       console.log('received chat data', chat)
       this.setState({chat})
     })
     this.onInputChange = this.onInputChange.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
+    this.onAvatarUrlInputChange = this.onAvatarUrlInputChange.bind(this)
+    this.toggleEditingAvatarUrl = this.toggleEditingAvatarUrl.bind(this)
+    this.setAvatarUrl = this.setAvatarUrl.bind(this)
+  }
+
+  setAvatarUrl() {
+    this.setState({
+      avatarUrl: this.state.avatarUrlInput,
+      editingAvatar: false,
+      avatarUrlInput: ''
+    })
+  }
+
+  toggleEditingAvatarUrl() {
+    this.setState({ editingAvatar: !(this.state.editingAvatar) })
+  }
+
+  onAvatarUrlInputChange(event) {
+    this.setState({ avatarUrlInput: event.target.value })
   }
 
   onInputChange(event) {
-    this.setState({input: event.target.value})
+    this.setState({ input: event.target.value })
   }
 
   onSubmit() {
     const body = this.state.input
-    const nickname = 'willis'
-    const avatarUrl = 'https://hwhome.com/content/images/thumbs/0005759_water-jug-man_600.jpeg'
+    const nickname = ''
+    const avatarUrl = this.state.avatarUrl
     socket.emit('data', { body, nickname, avatarUrl })
-    this.setState({input: ''})
+    this.setState({ input: '' })
   }
 
   render() {
@@ -42,6 +69,16 @@ class App extends React.Component {
           }
         </div>
         <div className="message-input">
+          { this.state.editingAvatar ?
+            <div>
+              <input value={this.state.avatarUrlInput} onChange={this.onAvatarUrlInputChange} />
+              <button onClick={this.setAvatarUrl}>done</button>
+            </div> :
+            <div>
+              <img ref={'my-avatar'} alt="avatar" className="avatar" src={this.state.avatarUrl} onError={(e) => {this.refs['my-avatar'].src = 'http://s3.amazonaws.com/37assets/svn/765-default-avatar.png'}} />
+              <button onClick={this.toggleEditingAvatarUrl}>edit</button>
+            </div>
+          }
           <textarea rows="7" value={this.state.input} onChange={this.onInputChange}/>
           <button onClick={this.onSubmit}>submit</button>
         </div>
